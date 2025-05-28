a7449980481914d2a10b1dd87d40e14c8d879d65
yomangit <212838158+yoms-den@users.noreply.github.com>
2025-05-28 11:38:31 +0800

    asfaf

--- resources/views/livewire/event-report/hazard-report/create-and-update.blade.php
+++ resources/views/livewire/event-report/hazard-report/create-and-update.blade.php
@@ -217,7 +217,7 @@
             </div>
         </fieldset>
         <div class="w-full max-w-md xl:max-w-xl form-control">
-            <x-label-no-req :value="__('documentation')" />
+            <x-label-req :value="__('documentation')" />
             <div class="relative">
                 <x-input-file wire:model.live='documentation' :error="$errors->get('documentation')" />
                 <div class="absolute inset-y-0 right-0 avatar" wire:target="documentation"
