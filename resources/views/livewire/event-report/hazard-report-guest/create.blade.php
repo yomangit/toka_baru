5760da804f0b910b814ab42c4b2581bc23e84782
yomangit <212838158+yoms-den@users.noreply.github.com>
2025-06-02 10:57:23 +0800

    sgdgs

--- resources/views/livewire/event-report/hazard-report-guest/create.blade.php
+++ resources/views/livewire/event-report/hazard-report-guest/create.blade.php
@@ -132,11 +132,11 @@
         </div>
         <fieldset>
             <x-label-no-req :value="__('kondisi tidak aman')" />
-            <input wire:model.live="kondisi_tidak _aman" value='yes' name="kondisi" id="aman"
+            <input wire:model.live="kondisi_tidak_aman" value='yes' name="kondisi" id="aman"
                 class="radio-xs peer/aman checked:bg-indigo-500 radio" type="radio" name="13" />
             <label for="aman"
                 class="text-xs font-semibold peer-checked/aman:text-indigo-500">{{ __('Yes') }}</label>
-            <input wire:model.live="kondisi_tidak _aman" value="no" id="tidak_aman"
+            <input wire:model.live="kondisi_tidak_aman" value="no" id="tidak_aman"
                 class="peer/tidak_aman radio-xs radio" type="radio" name="kondisi" />
             <label for="tidak_aman"
                 class="text-xs font-semibold peer-checked/tidak_aman:text-sky-500">{{ __('No') }}</label>
