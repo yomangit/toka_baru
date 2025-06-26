9b606fd0e148d3d6d9a84a9e5b4b95d475deae78
yomangit <212838158+yoms-den@users.noreply.github.com>
2025-06-26 11:40:40 +0800

    asasasasasssasas

--- resources/views/livewire/event-report/hazard-report/detail.blade.php
+++ resources/views/livewire/event-report/hazard-report/detail.blade.php
@@ -329,9 +329,9 @@
                     </div>
                 </div>
                 <div class="flex flex-col w-full border-opacity-50">
-                    <div class="tabs-lift tabs min-w-max">
+                    <div role="tablist" class="mb-4 tabs tabs-lifted">
                         <input type="radio" name="my_tabs_7" class="tab z-1" aria-label="Final Documentation" checked="checked" />
-                        <div class="sticky start-0 tab-content max-w-60 border-base-300 bg-base-100 p-6">
+                        <div role="tabpanel" class="p-6 tab-content bg-base-100 border-base-300 rounded-box">
                             <div class="mx-4 my-2">
                                 <x-btn-add data-tip="Add" class="{{ $currentStep === 'Closed' || $currentStep === 'Cancelled' ? 'btn-disabled ' : '' }}" wire:click="$dispatch('openModal', { component: 'event-report.hazard-report.documentation.create', arguments: { doc: {{ $data_id }} }})" />
                                 <livewire:event-report.hazard-report.documentation.index :id="$data_id">
