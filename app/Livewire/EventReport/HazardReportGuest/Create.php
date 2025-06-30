9d660c8bbe26a31a3ceb7d585bcba63782832105
yomangit <212838158+yoms-den@users.noreply.github.com>
2025-06-30 15:25:56 +0800

    asdasdadsd

--- app/Livewire/EventReport/HazardReportGuest/Create.php
+++ app/Livewire/EventReport/HazardReportGuest/Create.php
@@ -307,7 +307,7 @@ class Create extends Component
                 'line3'     => 'Thank you',
                 'actionUrl' => url("/eventReport/hazardReportDetail/$url"),
             ];
-            Notification::send($Users->first()->id, new toModerator($offerData));
+            Notification::send($this->report_to, new toModerator($offerData));
         $this->clearFields();
         // $this->redirectRoute('hazardReportCreate', ['workflow_template_id' => $this->workflow_template_id]);
     }
