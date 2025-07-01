a67d30db631e6335075d8518b0c67744c7369b5a
yomangit <212838158+yoms-den@users.noreply.github.com>
2025-07-01 12:20:44 +0800

    xcxcc

--- resources/views/livewire/event-report/hazard-report-guest/create.blade.php
+++ resources/views/livewire/event-report/hazard-report-guest/create.blade.php
@@ -173,8 +173,8 @@
             </div>
             <x-label-error :messages="$errors->get('immediate_corrective_action')" />
         </fieldset>
-        <div class="grid grid-rows-3 md:grid-rows-1 md:grid-cols-3 content-center md:gap-4 md:mt-2  divide-y-2 md:divide-x-2 divide-base-200 border  border-base-200 rounded-box">
-            <div class='mx-4 '>
+        <div class="grid grid-rows-3 md:grid-rows-1 md:grid-cols-3 content-center md:gap-4 mt-2  divide-y-2 md:divide-x-2 divide-base-200 border  border-base-200 rounded-box">
+            <div class='px-4 '>
                 <fieldset class="self-center w-40 max-w-sm fieldset rounded-box">
                     <label class="relative px-0 text-xs font-semibold capitalize label label-text-alt ">
                         {{ __('kondisi tidak aman') }}
@@ -182,7 +182,7 @@
                     </label>
                 </fieldset>
             </div>
-            <div class='mx-4'>
+            <div class='px-4'>
                 <fieldset class="self-center w-40 max-w-sm fieldset rounded-box">
                     <label class="relative px-0 text-xs font-semibold capitalize label label-text-alt ">
                         {{ __('Tindakan tidak aman') }}
@@ -190,7 +190,7 @@
                     </label>
                 </fieldset>
             </div>
-            <div class='mx-4'>
+            <div class='px-4'>
                 <fieldset class=" max-w-sm w-40 fieldset rounded-box ">
 
                     <x-label-req :value="__('perbaikan tingkat lanjut')" />
