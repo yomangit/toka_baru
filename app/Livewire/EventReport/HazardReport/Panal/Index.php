01148496a81ab1c681958f0ada4bd3f90f2ad5b0
yomangit <212838158+yoms-den@users.noreply.github.com>
2025-07-10 09:39:23 +0800

    asd

--- app/Livewire/EventReport/HazardReport/Panal/Index.php
+++ app/Livewire/EventReport/HazardReport/Panal/Index.php
@@ -62,7 +62,6 @@ class Index extends Component
             $Company = $ClassHierarchy->company_category_id;
             $Department = $ClassHierarchy->dept_by_business_unit_id;
             $User = (EventUserSecurity::where('user_id', Auth::user()->id)->where('responsible_role_id',  1)->where('type_event_report_id', $this->event_type_id)->exists()) ? EventUserSecurity::where('user_id', Auth::user()->id)->where('responsible_role_id',  1)->where('type_event_report_id', $this->event_type_id)->pluck('user_id') : EventUserSecurity::where('user_id', Auth::user()->id)->where('responsible_role_id',  1)->pluck('user_id');
-            dd($User);
             foreach ($User as $value) {
                 if (EventUserSecurity::where('user_id', $value)->searchCompany(trim($Company))->exists()) {
                     $this->muncul = true;
