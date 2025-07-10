19754a3e8f6d334695ab0d924a151f3cf48185b0
yomangit <212838158+yoms-den@users.noreply.github.com>
2025-07-10 11:31:36 +0800

    qwert

--- app/Livewire/EventReport/HazardReport/Panal/Index.php
+++ app/Livewire/EventReport/HazardReport/Panal/Index.php
@@ -73,10 +73,12 @@ class Index extends Component
         if ($ClassHierarchy) {
             $Company = $ClassHierarchy->company_category_id;
             $Department = $ClassHierarchy->dept_by_business_unit_id;
-            $Moderator = EventUserSecurity::where('user_id', Auth::user()->id)->where('responsible_role_id',  1)->where('type_event_report_id', $this->event_type_id)->exists();
+            $erm = EventUserSecurity::where('user_id', Auth::user()->id)->where('responsible_role_id',  2)->where('type_event_report_id', $this->event_type_id)->exists();
             $User = (EventUserSecurity::where('user_id', Auth::user()->id)->where('responsible_role_id',  1)->where('type_event_report_id', $this->event_type_id)->exists()) ? EventUserSecurity::where('user_id', Auth::user()->id)->where('responsible_role_id',  1)->where('type_event_report_id', $this->event_type_id)->pluck('user_id') : EventUserSecurity::where('user_id', Auth::user()->id)->where('responsible_role_id',  1)->pluck('user_id');
             if ($this->current_step === 'ERM Assigned') {
                 $this->muncul = false;
+            } elseif ($erm) {
+                $this->muncul = true;
             } else {
                 foreach ($User as $value) {
                     if (EventUserSecurity::where('user_id', $value)->searchCompany(trim($Company))->exists()) {
