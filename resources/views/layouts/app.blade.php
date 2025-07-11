08fb4642c2f5771a729ed006221a00a7a142aba7
yomangit <212838158+yoms-den@users.noreply.github.com>
2025-07-11 20:56:30 +0800

    asasas

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -69,27 +69,7 @@
             {{ $slot }}
         </main>
     </div>
-    <script src="https://cdn.onesignal.com/sdks/OneSignalSDK.js" async></script>
-    <script>
-        window.OneSignal = window.OneSignal || [];
-        OneSignal.push(function() {
-            OneSignal.init({
-                appId: "{{ env('ONESIGNAL_APP_ID') }}"
-                , notifyButton: {
-                    enable: true
-                }
-                , allowLocalhostAsSecureOrigin: true
-            , });
 
-            OneSignal.getUserId(function(userId) {
-                console.log("📡 OneSignal ID:", userId); // Wajib muncul di console
-                if (userId) {
-                    window.livewire.emit('saveOneSignalId', userId);
-                }
-            });
-        });
-
-    </script>
 
 
     @livewireScripts
