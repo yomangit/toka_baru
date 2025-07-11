8217ed51ee414728135afda0309e754c579ca91c
yomangit <212838158+yoms-den@users.noreply.github.com>
2025-07-11 22:28:16 +0800

    sdsd

--- app/Http/Middleware/AddContentSecurityPolicyHeaders.php
+++ app/Http/Middleware/AddContentSecurityPolicyHeaders.php
@@ -15,7 +15,7 @@ class AddContentSecurityPolicyHeaders
 
         $csp = implode('; ', [
             "default-src 'self'",
-            "script-src 'self' 'unsafe-eval' 'nonce-$nonce' https://cdn.onesignal.com https://onesignal.com https://cdn.ckeditor.com",
+            "script-src 'self'  'nonce-$nonce' https://cdn.onesignal.com https://onesignal.com https://cdn.ckeditor.com",
             "style-src 'self' 'unsafe-inline' https://cdn.jsdelivr.net https://fonts.googleapis.com",
             "font-src 'self' https://cdn.jsdelivr.net https://fonts.gstatic.com",
             "connect-src 'self' https://cdn.onesignal.com https://onesignal.com",
