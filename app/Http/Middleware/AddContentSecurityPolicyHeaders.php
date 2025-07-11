870af5f584264d93b46981d27db52b79e0c69a10
yomangit <212838158+yoms-den@users.noreply.github.com>
2025-07-11 22:57:58 +0800

    dfsdf

--- app/Http/Middleware/AddContentSecurityPolicyHeaders.php
+++ app/Http/Middleware/AddContentSecurityPolicyHeaders.php
@@ -15,7 +15,7 @@ class AddContentSecurityPolicyHeaders
 
         $csp = implode('; ', [
             "default-src 'self'",
-            "script-src 'self' 'unsafe-eval' 'nonce-$nonce' https://cdn.onesignal.com https://onesignal.com ",
+            "script-src 'self' 'nonce-$nonce' https://cdn.onesignal.com https://onesignal.com",
             "style-src 'self' 'unsafe-inline' https://cdn.jsdelivr.net https://fonts.googleapis.com",
             "font-src 'self' https://cdn.jsdelivr.net https://fonts.gstatic.com",
             "connect-src 'self' https://cdn.onesignal.com https://onesignal.com",
@@ -25,6 +25,7 @@ class AddContentSecurityPolicyHeaders
             "media-src 'self'",
             "object-src 'none'",
         ]);
+
 
 
         return $next($request)->withHeaders([
